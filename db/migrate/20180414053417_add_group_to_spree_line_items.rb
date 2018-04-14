83b14c016c1426b7df4184ef921ca334c8574604
qinghe <david@getstore.cn>
2018-04-14 13:48:23 +0800

    add migration group_number

--- db/migrate/20180414053417_add_group_to_spree_line_items.rb
+++ db/migrate/20180414053417_add_group_to_spree_line_items.rb
@@ -1,7 +1,10 @@
-class AddAddressIdToSpreeStores < ActiveRecord::Migration[5.1]
+class AddGroupToSpreeLineItems < ActiveRecord::Migration[5.1]
   def change
-    # https://github.com/vinsol-spree-contrib/spree_pickup
-    # each store has address as customer's order ship_address for POS
-    add_reference :spree_addresses, :store
+    # 订单生成，一个鞋子可能有两项服务，修鞋和洗鞋，即生成两条子订单，子订单是和服务项目相对应的。
+    # 同时生成条形码时，应为1个。即条形码是和服务物品相对应的。
+    add_column :spree_line_items, :group_number, :string, limit: 16
+    add_column :spree_line_items, :group_position, :integer
+    add_column :spree_shipments, :group_number, :string, limit: 16
+
   end
 end
