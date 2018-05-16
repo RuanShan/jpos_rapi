61e84e4526a1ae088d1c281b00156001c8fa474a
qinghe <david@getstore.cn>
2018-05-16 11:02:05 +0800

    调整spree_image 路径

--- app/models/spree/image.rb
+++ app/models/spree/image.rb
@@ -9,8 +9,8 @@ module Spree
     has_attached_file :attachment,
                       styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
                       default_style: :product,
-                      url: '/spree/products/:id/:style/:basename.:extension',
-                      path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
+                      url: '/images/spree/products/:id/:style/:basename.:extension',
+                      path: ':rails_root/public/images/spree/products/:id/:style/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
     validates_attachment :attachment,
                          presence: true,
