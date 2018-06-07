57a25024bfd500680c62206a08bbafc51a0d016e
qinghe <david@getstore.cn>
2018-06-07 12:03:06 +0800

    card_style 0: prepaid,  1:counts

--- app/models/selling/prepaid_card.rb
+++ app/models/selling/prepaid_card.rb
@@ -1,6 +1,6 @@
 module Selling
   class PrepaidCard < Spree::Product
-    default_scope { where( card_style: 1 ) }
+    default_scope { where( card_style: 0 ) }
     enum card_expire_in: { year: 0, month: 30, week: 7 }, _suffix: true
 
     [
--- app/models/selling/times_card.rb
+++ app/models/selling/times_card.rb
@@ -1,6 +1,6 @@
 module Selling
   class TimesCard < Spree::Product
-    default_scope { where( card_style: 2 ) }
+    default_scope { where( card_style: 1 ) }
     enum card_expire_in: { year: 365, month: 30, week: 7 }, _suffix: true
 
   end
