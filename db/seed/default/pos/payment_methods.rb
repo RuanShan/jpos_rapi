6f549b619b09d3b2352d9fd7669d74d2c3001cb2
qinghe <david@getstore.cn>
2018-06-09 16:28:59 +0800

    修正示例数据，添加payment_method.posable

--- db/seed/default/pos/payment_methods.rb
+++ db/seed/default/pos/payment_methods.rb
@@ -19,26 +19,30 @@ Spree::PaymentMethod::Cash.where(
   name: "现金",
   description: "现金支付",
   active: true,
-  auto_capture: true
+  auto_capture: true,
+  posable: true
 ).first_or_create!
 
 Spree::PaymentMethod::Weixin.where(
   name: "微信",
   description: "微信支付",
   active: true,
-  auto_capture: true
+  auto_capture: true,
+  posable: true
 ).first_or_create!
 
 Spree::PaymentMethod::Alipay.where(
   name: "支付宝",
   description: "支付宝",
   active: true,
-  auto_capture: true
+  auto_capture: true,
+  posable: true
 ).first_or_create!
 
 Spree::PaymentMethod::Unionpay.where(
   name: "银联",
   description: "银联支付",
   active: true,
-  auto_capture: true
+  auto_capture: true,
+  posable: true
 ).first_or_create!
