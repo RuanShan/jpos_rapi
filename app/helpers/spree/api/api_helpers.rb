beeb1525b1daa7dd7e178cd2a58013f84f269b03
qinghe <david@getstore.cn>
2018-06-16 20:33:08 +0800

    添加客户属性 customer.store_id

--- app/helpers/spree/api/api_helpers.rb
+++ app/helpers/spree/api/api_helpers.rb
@@ -141,7 +141,8 @@ module Spree
 
       @@country_attributes = [:id, :iso_name, :iso, :iso3, :name, :numcode]
 
-      @@customer_attributes = [:id, :created_at, :updated_at, :username, :mobile, :address, :birth, :memo, :payment_password, :gender]
+      @@customer_attributes = [:id, :created_at, :updated_at, :username,
+        :mobile, :address, :birth, :memo, :payment_password, :gender, :store_id]
 
       @@state_attributes = [:id, :name, :abbr, :country_id]
 
