087940a24ee6a90b97979e3822056f59324a91bf
qinghe <david@getstore.cn>
2018-06-22 14:10:55 +0800

    支持订单的创建时间为查询条件

--- app/models/spree/order.rb
+++ app/models/spree/order.rb
@@ -51,7 +51,7 @@ module Spree
     end
 
     self.whitelisted_ransackable_associations = %w[shipments user promotions bill_address ship_address line_items line_item_groups payments store]
-    self.whitelisted_ransackable_attributes = %w[store_id completed_at email number state payment_state group_state shipment_state total order_type]
+    self.whitelisted_ransackable_attributes = %w[store_id completed_at email number state payment_state group_state shipment_state total order_type created_at]
 
     attr_reader :coupon_code
     attr_accessor :temporary_address, :temporary_credit_card
