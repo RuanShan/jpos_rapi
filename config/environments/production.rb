40d58e809f7ca6ab5ad19f2cdcefb56bbee6f658
qinghe <david@getstore.cn>
2018-07-14 12:27:37 +0800

    删除 ssl配置

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,9 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-
-  config.force_ssl = true
 end
 
 Paperclip.interpolates :images_host  do |attachment, style|
