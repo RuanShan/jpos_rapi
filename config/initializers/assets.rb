a5e8eb4682235ce40ad5b51aac20f2060b969517
qinghe <david@getstore.cn>
2018-07-16 19:40:34 +0800

    admin 工作

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
 Rails.application.config.assets.precompile += %w(logo/spree_50.png noimage/*.png)
 
 Rails.application.config.assets.precompile += %w(admin/* credit_cards/credit_card.gif)
+
+Rails.application.config.assets.precompile += %w(spree/all.js spree/backend.css)
