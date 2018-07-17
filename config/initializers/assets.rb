c14ba849a50b66f580099512bf0f3b4cbdb357db
qinghe <david@getstore.cn>
2018-07-17 11:14:59 +0800

    尝试修正spree/admin css丢失bootstrap变量问题

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@ Rails.application.config.assets.precompile += %w(logo/spree_50.png noimage/*.png
 
 Rails.application.config.assets.precompile += %w(admin/* credit_cards/credit_card.gif)
 
-Rails.application.config.assets.precompile += %w(spree/all.js spree/backend.css)
+Rails.application.config.assets.precompile += %w(spree/all.js spree/backend.scss)
