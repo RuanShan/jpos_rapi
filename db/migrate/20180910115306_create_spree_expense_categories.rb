4d240410ce7328787077a8bf53a1dead9af7bcdb
qinghe <david@getstore.cn>
2018-09-11 16:29:16 +0800

    change table charset

--- db/migrate/20180910115306_create_spree_expense_categories.rb
+++ db/migrate/20180910115306_create_spree_expense_categories.rb
@@ -1,7 +1,7 @@
 class CreateSpreeExpenseCategories < ActiveRecord::Migration[5.1]
   def change
     #服务员/店铺一天的运营费用
-    create_table "spree_expense_categories"  do |t|
+    create_table "spree_expense_categories" , options: "DEFAULT CHARSET=utf8mb4" do |t|
       t.string "name"
       t.string "description"
       t.boolean "is_default", default: false
