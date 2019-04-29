359cc18f60c4b6eea05b575bd91449644ab3327a
qinghe <david@getstore.cn>
2019-04-29 16:19:54 +0800

    修正iPad上传图片丢图问题

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -3,3 +3,19 @@ class ActiveStorage::PurgeJob < ActiveStorage::BaseJob
     blob.purge unless blob.attachments.present?
   end
 end
+
+
+ActiveStorage::Service::AliyunService.class_eval do
+  # ipad 上传图片随机出现不成功。 提示 InvalidDigest
+  # https://www.alibabacloud.com/help/zh/doc-detail/32005.htm
+  def headers_for_direct_upload(key, content_type:, checksum:, **)
+    checksum = ''
+    date = Time.now.httpdate
+    {
+      "Content-Type" => content_type,
+      "Content-MD5" => checksum,
+      "Authorization" => authorization(key, content_type, checksum, date),
+      "x-oss-date" => date,
+    }
+  end
+end
