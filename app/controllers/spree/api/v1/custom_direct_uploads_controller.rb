a0143af9193d6be572bc64073b9ccfbce7044bae
qinghe <david@getstore.cn>
2019-04-29 15:40:43 +0800

    Update custom_direct_uploads_controller.rb

--- app/controllers/spree/api/v1/custom_direct_uploads_controller.rb
+++ app/controllers/spree/api/v1/custom_direct_uploads_controller.rb
@@ -15,7 +15,7 @@ module Spree
 
           def direct_upload_json(blob)
             # ipad 上传图片随机出现不成功。
-            headers_except_content_md5	 =  blob.service_headers_for_direct_upload.except("Content-Md5")
+            headers_except_content_md5	 =  blob.service_headers_for_direct_upload.except("Content-MD5")
             blob.as_json(root: false, methods: :signed_id).merge(direct_upload: {
               url: blob.service_url_for_direct_upload( ),
               headers: headers_except_content_md5
