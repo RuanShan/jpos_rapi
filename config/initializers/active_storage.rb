2992e8dea841e38d9a87b702c988807816b1f905
qinghe <david@getstore.cn>
2019-04-30 11:43:29 +0800

    enable content-md5

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -7,17 +7,17 @@ class ActiveStorage::PurgeJob < ActiveStorage::BaseJob
 end
 
 #
-ActiveStorage::Service::AliyunService.class_eval do
-  # ipad 上传图片随机出现不成功。 提示 InvalidDigest
-  # https://www.alibabacloud.com/help/zh/doc-detail/32005.htm
-  def headers_for_direct_upload(key, content_type:, checksum:, **)
-    checksum = ''
-    date = Time.now.httpdate
-    {
-      "Content-Type" => content_type,
-      #"Content-MD5" => checksum,
-      "Authorization" => authorization(key, content_type, checksum, date),
-      "x-oss-date" => date,
-    }
-  end
-end
+# ActiveStorage::Service::AliyunService.class_eval do
+#   # ipad 上传图片随机出现不成功。 提示 InvalidDigest
+#   # https://www.alibabacloud.com/help/zh/doc-detail/32005.htm
+#   def headers_for_direct_upload(key, content_type:, checksum:, **)
+#     checksum = ''
+#     date = Time.now.httpdate
+#     {
+#       "Content-Type" => content_type,
+#       "Content-MD5" => checksum,
+#       "Authorization" => authorization(key, content_type, checksum, date),
+#       "x-oss-date" => date,
+#     }
+#   end
+# end
