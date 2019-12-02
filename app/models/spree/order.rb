6d43dd6e887412ed01f57c0c227c7d00befab2da
qinghe <david@getstore.cn>
2019-12-02 22:46:20 +0800

    更新日销售计算方式

--- app/models/spree/order.rb
+++ app/models/spree/order.rb
@@ -550,12 +550,15 @@ module Spree
             count = Spree::Order.in_day(  self.created_at ).where( created_by_id: self.created_by_id, store_id: self.store_id ).type_normal.with_state(:cart).count
             self.sale_day.service_order_count = count
             if self.payment_at.present?
-              service_total =  Spree::Order.in_day( self.created_at ).where( created_by_id: self.created_by_id, store_id: self.store_id, order_type: [:normal] ).with_state(:cart).sum(:payment_total)
+              payment_sale_day = SaleDay.on_date( self.payment_at ).where( user_id: self.created_by_id, store_id: self.store_id).first
+              service_total =  Spree::Order. where( ["payment_at>? and payment_at<?",  self.payment_at.beginning_of_day, self.payment_at.end_of_day] ).where( created_by_id: self.created_by_id, store_id: self.store_id, order_type: [:normal] ).with_state(:cart).sum(:payment_total)
               # 当天收入是多少, 包括现金、支付宝、微信、pos？
               method_ids = Spree::PaymentMethod.where( posable: true ).pluck(:id)
               service_posable_total = Spree::Payment.completed.in_day(self.payment_at).joins(:order).where( payment_method_id: method_ids, spree_orders:{ created_by_id: self.created_by_id, store_id: self.store_id, order_type: [:normal], state: :cart }).sum(:amount)
-              self.sale_day.service_total = service_total
-              self.sale_day.service_posable_total = service_posable_total
+              # 找到 payment_at 对应的sale_day, 更新
+              payment_sale_day.service_total = service_total
+              payment_sale_day.service_posable_total = service_posable_total
+              payment_sale_day.save!
             end
         end
         self.sale_day.save!
