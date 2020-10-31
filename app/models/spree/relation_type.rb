9fe75ca50201ef1893a82a868d2755dc5f040931
qinghe <david@getstore.cn>
2020-10-31 14:03:21 +0800

    Update relation_type.rb

--- app/models/spree/relation_type.rb
+++ app/models/spree/relation_type.rb
@@ -2,5 +2,5 @@ class Spree::RelationType < ActiveRecord::Base
   has_many :relations, dependent: :destroy
 
   validates :name, :applies_to, presence: true
-  validates :name, uniqueness: { case_sensitive: false }, scope: :site_id
+  validates :name, uniqueness: { case_sensitive: false, scope: :site_id }
 end
